da74c99bb9b5350d09912cefeecb991b6d6ccad4
Andy Nadvorny <andynadvorny@gmail.com>
2022-08-03 18:08:40 -0300

    aula 08: trabalhando com CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,71 @@
+import Head from 'next/head'
+import Link from 'next/link';
+import {  RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic'
+
+import styles from './styles.module.scss'
+
+type Post ={
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({posts} :PostProps){
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            // eslint-disable-next-line react/jsx-key
+            <Link href={`/posts/preview/${post.slug}`}>
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+               {post.excerpt}
+              </p>
+            </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.getByType("post", {
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post =>{
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'en-US', {day: '2-digit',
+         month: 'short', 
+         year: 'numeric'
+        })
+      }
+    });
+  
+  return {
+    props: { posts },
+  };
+}
+
